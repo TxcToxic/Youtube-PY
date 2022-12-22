af9f2697b73912073cd97b0700552c1626c36f2e
-TOXIC- <93470009+TxcToxic@users.noreply.github.com>
2022-12-22 11:11:47 +0100

    Update main.py

--- Package/main.py
+++ Package/main.py
@@ -5,5 +5,5 @@ json_data = {
     "username": "Llama Dude"
 }
 
-print(tutorial.webhookSend("https://discord.com/api/webhooks/1055411133432987668/oIB_wSkg1TvZcoMsezyFSrOVuT9ob9kfMxtHuK0kmSbFmDSfOlzE33G7N741fJEN2o3L",
+print(tutorial.webhookSend("https://discord.com/api/webhooks/",
                            json_data))
